2a4bd1803ed7de2f8ae757a5b0ac4a77f3a48bd7
charlesmatte <matte.charles@gmail.com>
2023-11-15 21:59:09 -0500

    Created auction service

---
+++ src/AuctionService/Data/Migrations/20231116013714_RenamedItemsTable.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedItemsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tables_Auctions_AuctionId",
+                table: "Tables");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tables",
+                table: "Tables");
+
+            migrationBuilder.RenameTable(
+                name: "Tables",
+                newName: "Items");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tables_AuctionId",
+                table: "Items",
+                newName: "IX_Items_AuctionId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Items",
+                table: "Items",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Auctions_AuctionId",
+                table: "Items",
+                column: "AuctionId",
+                principalTable: "Auctions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Auctions_AuctionId",
+                table: "Items");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Items",
+                table: "Items");
+
+            migrationBuilder.RenameTable(
+                name: "Items",
+                newName: "Tables");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Items_AuctionId",
+                table: "Tables",
+                newName: "IX_Tables_AuctionId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tables",
+                table: "Tables",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tables_Auctions_AuctionId",
+                table: "Tables",
+                column: "AuctionId",
+                principalTable: "Auctions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
